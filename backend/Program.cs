2064e18ddae2ffa19ab98719d290e5aef732584a
Damian <damian.pillay@kcpivot.co.za>
2025-05-15 15:04:55 +0200

    feat: add simple project repository logic using EF

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,18 @@
+using backend.Data;
+using backend.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ backend/Repositories/ProjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using backend.Data;
+using backend.Models;
+
+namespace backend.Repositories;
+
+public class ProjectRepository : IProjectRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProjectRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Project> GetAllProjects()
+    {
+        return _context.Projects.ToList();
+    }
+}
