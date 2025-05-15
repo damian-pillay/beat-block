968bd58d244c9c719e9e0600fcf806a5a226f619
Damian <damian.pillay@kcpivot.co.za>
2025-05-15 16:11:53 +0200

    fix: temporarily remove user Id field

--- backend/Models/Project.cs
+++ backend/Models/Project.cs
@@ -3,7 +3,6 @@
     public class Project
     {
         public int ProjectId { get; set; }
-        public int UserId { get; set; }
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
         public string? KeySignature { get; set; }
