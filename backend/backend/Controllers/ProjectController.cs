c5e54d8ab4ae33382cb45bf350f6b9d1af5ffa9f
Damian <damian.pillay@kcpivot.co.za>
2025-05-16 09:23:24 +0200

    feat: add project controller

--- backend/backend/Controllers/ProjectController.cs
+++ backend/backend/Controllers/ProjectController.cs
@@ -1,5 +1,24 @@
-﻿namespace BeatBlock.Controllers;
+﻿using BeatBlock.Models;
+using BeatBlock.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class ProjectController
+namespace BeatBlock.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectController : ControllerBase
 {
+    private readonly IProjectService _projectService;
+
+    public ProjectController(IProjectService projectService)
+    {
+        _projectService = projectService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Project>> GetAll()
+    {
+        var projects = _projectService.GetAllProjects();
+        return Ok(projects);
+    }
 }
