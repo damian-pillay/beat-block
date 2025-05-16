3a7f30db85cc55214953e47ee9d1d4381e5b4dc1
Damian <damian.pillay@kcpivot.co.za>
2025-05-16 11:52:28 +0200

    feat: add data transfer object for project

---
+++ backend/backend/DTOs/ProjectDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeatBlock.DTOs;
+
+public class ProjectDTO
+{
+    [Required]
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? KeySignature { get; set; }
+    public byte? Bpm { get; set; }
+    public string? Genre { get; set; }
+    
+    [Required]
+    public string Daw { get; set; } = null!;
+
+    [Required]
+    public string FilesUrl { get; set; } = null!;
+    public string? AudioUrl { get; set; }
+    public string? ArtworkUrl { get; set; }
+}
