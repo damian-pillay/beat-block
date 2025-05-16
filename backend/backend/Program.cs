063c817b07f843286bad5b193c77d4b7334e5eba
Damian <damian.pillay@kcpivot.co.za>
2025-05-16 09:24:47 +0200

    feat: add necessary dependencies to application

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -2,8 +2,18 @@ using BeatBlock.Data;
 using BeatBlock.Repositories;
 using BeatBlock.Services;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+var sqlPassword = Environment.GetEnvironmentVariable("SQL_PASSWORD");
+var sqlServer = Environment.GetEnvironmentVariable("SQL_SERVER");
+var sqlPort = Environment.GetEnvironmentVariable("SQL_PORT");
+var sqlDb = Environment.GetEnvironmentVariable("SQL_DB");
+
+var connectionString = $"Server={sqlServer},{sqlPort};Database={sqlDb};User Id=sa;Password={sqlPassword};TrustServerCertificate=True;";
 
 // Add services to the container.
 
@@ -15,6 +25,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -25,6 +45,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
