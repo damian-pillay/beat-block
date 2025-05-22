57a1f1989adb415cfb5193250778f4d7ce56cdf9
Damian <damian.pillay@kcpivot.co.za>
2025-05-22 11:03:39 +0200

    fix: change naming to correspond with new db field

--- backend/backend/Models/Project.cs
+++ backend/backend/Models/Project.cs
@@ -2,7 +2,7 @@
 {
     public class Project
     {
-        public int ProjectId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
         public string? KeySignature { get; set; }
