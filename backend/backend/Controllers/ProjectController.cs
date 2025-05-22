a8bf56d3ce8a73acf3d5810a21f2bcc413e02781
Damian <damian.pillay@kcpivot.co.za>
2025-05-22 11:03:07 +0200

    feat: add request for deleting project by id

--- backend/backend/Controllers/ProjectController.cs
+++ backend/backend/Controllers/ProjectController.cs
@@ -40,15 +40,35 @@ public class ProjectController : ControllerBase
         
         var createdProject = await _projectService.CreateProjectAsync(projectDto);
 
-        return CreatedAtAction(nameof(GetProjectById), new { id = createdProject.ProjectId }, createdProject);
+        return CreatedAtAction(nameof(GetProjectById), new { id = createdProject.Id }, createdProject);
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetProjectById(int id)
     {
+        Console.WriteLine($"Getting project by ID: {id}");
         var project = await _projectService.GetProjectByIdAsync(id);
-        if (project == null) return NotFound();
+        if (project == null)
+        {
+            Console.WriteLine("Project not found");
+            return NotFound();
+        }
 
         return Ok(project);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProject(int id)
+    {
+        Console.WriteLine($"Deleting project by ID: {id}");
+        var result = await _projectService.DeleteProjectAsync(id);
+
+        if (!result)
+        {
+            Console.WriteLine($"Project not found");
+            return NotFound();
+        }
+            
+        return NoContent(); // 204 response
     }
 }
