42018b55b39742927eb24d79ac8f287d9f52bb54
Damian <damian.pillay@kcpivot.co.za>
2025-05-27 15:14:21 +0200

    Revert "chore: remove unused helper class"
    This reverts commit fd5cb12af8b101b7a5490997e42af0328bec8e2b.

---
+++ backend/backend-test/Helpers/FormFileHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+
+public static class FormFileHelper
+{
+    public static IFormFile CreateFakeFormFile(string content, string fileName = "file.txt", string contentType = "application/octet-stream")
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+        return new FormFile(stream, 0, stream.Length, "file", fileName)
+        {
+            Headers = new HeaderDictionary(),
+            ContentType = contentType
+        };
+    }
+}
