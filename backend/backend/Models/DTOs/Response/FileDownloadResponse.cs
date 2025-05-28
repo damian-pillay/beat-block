c13ff7a03a24f1ca600e9d3ad61281a93170a742
Damian <damian.pillay@kcpivot.co.za>
2025-05-28 11:58:22 +0200

    feat: add content type to DTO

--- backend/backend/Models/DTOs/Response/FileDownloadResponse.cs
+++ backend/backend/Models/DTOs/Response/FileDownloadResponse.cs
@@ -4,4 +4,5 @@ public record FileDownloadResponse
 {
     public Stream FileStream { get; init; } = null!;
     public string FileName { get; init; } = null!;
+    public string ContentType { get; init; } = null!;
 }
