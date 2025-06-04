98706508731a0380f04fe2cd168e50ee1d29a329
Damian <damian.pillay@kcpivot.co.za>
2025-06-04 14:33:24 +0200

    feat: add global exception handler class

---
+++ backend/backend/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using BeatBlock.Helpers;
+
+namespace BeatBlock.Middleware;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "An unhandled exception occurred.");
+
+        httpContext.Response.StatusCode = 500;
+        httpContext.Response.ContentType = "application/json";
+
+        var (status, title, detail, type) =
+            ProjectExceptionMap.ExceptionToResponse.TryGetValue(exception.GetType(), out var value)
+            ? value
+            : ProjectExceptionMap.DefaultResponse;
+
+        var errorResponse = new
+        {
+            type = type,
+            title = title,
+            status = status,
+            detail = detail, 
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(errorResponse, cancellationToken);
+        return true;
+    }
+}
