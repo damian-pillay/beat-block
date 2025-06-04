540db7b09f6fd1476e0993419b34ee361eba5faa
Damian <damian.pillay@kcpivot.co.za>
2025-06-04 11:41:23 +0200

    feat: change Project from record to class

--- backend/backend/Models/Project.cs
+++ backend/backend/Models/Project.cs
@@ -1,18 +1,17 @@
-﻿namespace BeatBlock.Models
+﻿namespace BeatBlock.Models;
+
+public class Project
 {
-    public record Project
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string? Description { get; set; }
-        public string? KeySignature { get; set; }
-        public byte? Bpm { get; set; }
-        public string? Genre { get; set; }
-        public string Daw { get; set; } = null!;
-        public string FilePath { get; set; } = null!;
-        public string? AudioPath { get; set; }
-        public string? ImagePath { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? KeySignature { get; set; }
+    public byte? Bpm { get; set; }
+    public string? Genre { get; set; }
+    public string Daw { get; set; } = null!;
+    public string FilePath { get; set; } = null!;
+    public string? AudioPath { get; set; }
+    public string? ImagePath { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
