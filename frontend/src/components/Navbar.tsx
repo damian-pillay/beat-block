86f1fc87e73eea81e2c102b6226407ad35d3311c
Damian <damian.pillay@kcpivot.co.za>
2025-06-05 14:52:18 +0200

    feat: add basic nav bar to the top of the page

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import BeatblockLogo from "../assets/BEATBLOCK.svg";
+import MenuIcon from "../assets/menu.svg";
+import UploadIcon from "../assets/upload.svg";
+import GridTexture from "../assets/grid-texture.svg";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="w-full flex items-center justify-between px-8 py-4 bg-[#171515] select-none">
+      <button className="text-white text-2xl" aria-label="Menu">
+        <img src={MenuIcon} alt="Menu Icon" className="h-8 w-8" />
+      </button>
+
+      <img src={BeatblockLogo} alt="BEATBLOCK Logo" className="h-18" />
+
+      <button className="text-white text-2xl" aria-label="Upload">
+        <img src={UploadIcon} alt="Upload Icon" className="h-8 w-8" />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
