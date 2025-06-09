89be7c0259e3a694c0dbafd81ac100d9027c272f
Damian <damian.pillay@kcpivot.co.za>
2025-06-09 10:00:17 +0200

    feat: improve formatting and style

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,14 @@
 import ActionButton from "./Buttons/ActionButton";
 import SearchBar from "./SearchBar";
+import GreetingMessage from "./GreetingMessage";
 
 export default function Header() {
   return (
-    <div className="relative overflow-hidden mb-10">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-montserrat font-extrabold text-white text-center pt-10">
-        Good Afternoon, Damian.
-      </h1>
-      <div className="flex items-center justify-center pt-4">
+    <div className="flex flex-col gap-4 relative pb-6 pt-6">
+      <GreetingMessage userFirstName="Damian" />
+      <div className="flex gap-1.5 items-center w-[90%] mx-auto justify-center max-w-[85%] md:w-3xl">
         <SearchBar />
-        <div className="ml-2 flex space-x-2">
+        <div className="flex gap-1 w-fit">
           <ActionButton icon="filter_alt" ariaLabel="Filter" />
           <ActionButton icon="sort" ariaLabel="Sort" />
         </div>
