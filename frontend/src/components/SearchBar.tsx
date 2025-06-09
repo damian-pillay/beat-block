e878fbc275615ed157b6d42a0b99923ebb327f3e
Damian <damian.pillay@kcpivot.co.za>
2025-06-09 15:07:43 +0200

    fix: adjust search bar interaction colour to be consistent with icons

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 export default function SearchBar() {
   return (
-    <div className="flex items-center w-full bg-[#272626] rounded-4xl px-4 h-full hover:bg-[#3a3a3a] transition focus-within:bg-[#3a3a3a]">
+    <div className="flex items-center w-full bg-[#272626] rounded-4xl px-4 h-full hover:bg-[#383737] transition focus-within:bg-[#383737]">
       <span className="material-icons text-gray-400 mr-3 select-none pointer-events-none">
         search
       </span>
