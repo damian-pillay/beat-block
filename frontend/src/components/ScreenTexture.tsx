d75e99516c26ea40fa1547e5994da358171956cf
Damian <damian.pillay@kcpivot.co.za>
2025-06-09 09:59:48 +0200

    feat: add more screen textures and improve style

--- frontend/src/components/ScreenTexture.tsx
+++ frontend/src/components/ScreenTexture.tsx
@@ -1,5 +1,6 @@
 import GridTexture from "../assets/grid-texture.svg";
 import ArrowTexture from "../assets/arrow-texture.svg";
+import LineTexture from "../assets/line-texture.svg";
 
 export default function ScreenTexture() {
   return (
@@ -16,6 +17,7 @@ export default function ScreenTexture() {
           select-none 
           overflow-hidden
           [@media(max-width:1000px)]:hidden
+          opacity-40
         "
       />
 
@@ -32,19 +34,39 @@ export default function ScreenTexture() {
           md:w-60 
           pointer-events-none 
           select-none
-          overflow-hidden"
+          overflow-hidden
+          opacity-40"
       />
 
       <img
         src={ArrowTexture}
-        alt="Noise Texture"
+        alt="Arrow Texture"
         className="
           absolute 
-          top-[220px]
+          top-[270px]
           left-[8vw] 
           md:w-20 
           sm:w-15 
-          w-[0] 
+          w-15
+          pointer-events-none 
+          select-none
+          opacity-40
+          "
+      />
+
+      <img
+        src={LineTexture}
+        alt="Line Texture"
+        className="
+          absolute 
+          bottom-[2vw]
+          left-1/2
+          transform 
+          -translate-x-1/2
+          opacity-20
+          md:w-25
+          sm:w-20 
+          w-15
           pointer-events-none 
           select-none
           "
