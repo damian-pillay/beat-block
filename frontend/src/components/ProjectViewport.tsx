600616e4da8ddded60f22a9829f5319b3cbe26d6
Damian <damian.pillay@kcpivot.co.za>
2025-06-09 15:08:49 +0200

    feat: display projectblock within viewport

--- frontend/src/components/ProjectViewport.tsx
+++ frontend/src/components/ProjectViewport.tsx
@@ -1,11 +1,19 @@
+import ProjectBlock from "./ProjectBlock";
+
 type ProjectViewportProps = {
   content: any;
 };
 
 export default function ProjectViewport({ content }: ProjectViewportProps) {
   return (
-    <div className="flex flex-col max-w-[80%] w-6xl mx-auto h-full max-h-[90%] overflow-auto scrollbar-hide">
-      <pre>{JSON.stringify(content, null, 2)}</pre>
+    <div className="grid grid-cols-2 [@media(max-width:1280px)]:grid-cols-1 items-start md:auto-rows-[12rem] auto-rows-[9.5rem] gap-3 max-w-[90%] w-6xl mx-auto h-full overflow-auto scrollbar-hide">
+      {Array.isArray(content?.projects) && content.projects.length > 0 ? (
+        content.projects.map((project: any, index: number) => (
+          <ProjectBlock key={project.id || index} project={project} />
+        ))
+      ) : (
+        <p>No Projects Found</p>
+      )}
     </div>
   );
 }
