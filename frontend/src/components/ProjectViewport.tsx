a5de4bff8c0f6beb4f67d51d8de4c6ecdf1ff1c9
Damian <damian.pillay@kcpivot.co.za>
2025-06-10 10:38:16 +0200

    fix: adjust the style to allow for proper displaying of hover shadow

--- frontend/src/components/ProjectViewport.tsx
+++ frontend/src/components/ProjectViewport.tsx
@@ -1,4 +1,4 @@
-import ProjectBlock from "./ProjectBlock";
+import ProjectBlock from "./ProjectBlock/ProjectBlock";
 
 type ProjectViewportProps = {
   content: any;
@@ -6,7 +6,7 @@ type ProjectViewportProps = {
 
 export default function ProjectViewport({ content }: ProjectViewportProps) {
   return (
-    <div className="grid grid-cols-2 [@media(max-width:1280px)]:grid-cols-1 items-start md:auto-rows-[12rem] auto-rows-[9.5rem] gap-3 max-w-[90%] w-6xl mx-auto h-full overflow-auto scrollbar-hide">
+    <div className="grid grid-cols-2 [@media(max-width:1280px)]:grid-cols-1 items-start md:auto-rows-[12rem] auto-rows-[9.5rem] gap-3 max-w-[100%] mx-auto h-full overflow-auto scrollbar-hide md:px-10 px-8 py-7">
       {Array.isArray(content?.projects) && content.projects.length > 0 ? (
         content.projects.map((project: any, index: number) => (
           <ProjectBlock key={project.id || index} project={project} />
