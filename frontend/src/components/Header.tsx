2d6fc66047e59944232ddccf2ebf006b1c85bb50
Damian <damian.pillay@kcpivot.co.za>
2025-06-10 10:39:22 +0200

    fix: adjust padding

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import GreetingMessage from "./GreetingMessage";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-4 relative pb-6 pt-6">
+    <div className="flex flex-col gap-4 relative pb-5 pt-6">
       <GreetingMessage userFirstName="Damian" />
       <div className="flex gap-1.5 items-center w-[90%] mx-auto justify-center max-w-[85%] md:w-3xl">
         <SearchBar />
