e3698f04ee8fc335f45493632bd938bfc2d953a1
Damian <damian.pillay@kcpivot.co.za>
2025-06-11 08:05:50 +0200

    feat: make action buttons none selectable

--- frontend/src/components/common/Buttons/ActionButton.tsx
+++ frontend/src/components/common/Buttons/ActionButton.tsx
@@ -11,7 +11,7 @@ export default function ActionButton({
 }: ActionButtonProps) {
   return (
     <button
-      className="bg-[#272626] text-white py-2.5 px-3 rounded-full hover:bg-[#383737] transition focus:outline-none cursor-pointer"
+      className="bg-[#272626] text-white py-2.5 px-3 rounded-full hover:bg-[#383737] transition focus:outline-none cursor-pointer select-none"
       aria-label={ariaLabel}
       onClick={onClick}
     >
