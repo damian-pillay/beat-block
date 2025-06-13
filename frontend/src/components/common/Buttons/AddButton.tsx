40b24aab3f338fa7554f2eba7594c19420189d3c
D4M14N L33 <damian.pillay@kcpivot.co.za>
2025-06-13 08:28:39 +0200

    feat: implement zustand state management on Add button

--- frontend/src/components/common/Buttons/AddButton.tsx
+++ frontend/src/components/common/Buttons/AddButton.tsx
@@ -1,22 +1,30 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useModalStore } from "../../../store/useModalStore";
 
 export default function AddButton() {
-  const [openModal, setOpenModal] = useState(false);
+  const isModalOpen = useModalStore((state) => state.isOpen);
+  const openModal = useModalStore((state) => state.openModal);
+  const closeModal = useModalStore((state) => state.closeModal);
+
   function handleToggle() {
-    setOpenModal((prev) => !prev);
+    if (isModalOpen) {
+      closeModal();
+    } else {
+      openModal();
+    }
   }
 
   return (
     <motion.button
       onClick={handleToggle}
-      className="cursor-pointer stroke-white rounded-full h-full aspect-square flex items-center justify-center bg-[#ff0000]"
+      animate={{ backgroundColor: isModalOpen ? "#272626" : "#ff0000" }}
+      className="cursor-pointer stroke-white rounded-full h-full aspect-square flex z-50 items-center justify-center"
     >
       <motion.div
         className="flex justify-center items-center w-[80%] h-[80%]"
         animate={{ rotate: 0 }}
         whileHover={
-          openModal
+          isModalOpen
             ? {
                 rotate: [-4, 4, -4, 4, -4],
                 transition: {
@@ -36,7 +44,7 @@ export default function AddButton() {
           stroke="white"
           strokeWidth="1.8"
           strokeLinecap="round"
-          animate={{ rotate: openModal ? 45 : 0 }}
+          animate={{ rotate: isModalOpen ? 45 : 0 }}
           transition={{ duration: 0.3 }}
         >
           <line x1="5" y1="12" x2="19" y2="12" />
