459a1d7558112e2667c0a36bb2ac40982296aaa3
D4M14N L33 <damian.pillay@kcpivot.co.za>
2025-06-17 08:17:31 +0200

    fix: set text to left align

--- frontend/src/components/common/ProjectBlock/ProjectDescription.tsx
+++ frontend/src/components/common/ProjectBlock/ProjectDescription.tsx
@@ -7,7 +7,7 @@ export default function ProjectDescription({
 }) {
   return (
     <section className="flex flex-col font-montserrat justify-between w-full flex-grow">
-      <h3 className="font-montserrat font-extrabold text-white md:text-xl text-[1em]">
+      <h3 className="text-left font-montserrat font-extrabold text-white md:text-xl text-[1em]">
         {name}
       </h3>
       <p className="font-montserrat font-normal h-[90%] text-[#848484] md:text-sm text-[0.55em] text-justify">
