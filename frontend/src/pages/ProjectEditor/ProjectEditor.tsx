67bce76d112550caf75a28b20b4a706be9423ee7
D4M14N L33 <damian.pillay@kcpivot.co.za>
2025-06-17 14:06:54 +0200

    feat: create project editor page

---
+++ frontend/src/pages/ProjectEditor/ProjectEditor.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/common/NavBar/Navbar";
+import ScreenTexture from "../../components/layout/ScreenTexture";
+import EditorModal from "./EditorModal";
+
+function ProjectEditor() {
+  return (
+    <>
+      <div className="relative h-screen w-screen flex flex-col">
+        <ScreenTexture />
+        <Navbar />
+        <EditorModal />
+      </div>
+    </>
+  );
+}
+
+export default ProjectEditor;
