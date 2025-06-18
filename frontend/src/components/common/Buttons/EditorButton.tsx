42c5749b557235c8062b8f4553b722d494ed0923
D4M14N L33 <damian.pillay@kcpivot.co.za>
2025-06-18 10:09:25 +0200

    feat: add on click logic to button
    - and fixed the form button type bug

--- frontend/src/components/common/Buttons/EditorButton.tsx
+++ frontend/src/components/common/Buttons/EditorButton.tsx
@@ -1,12 +1,19 @@
 import { motion } from "framer-motion";
+import { useEditorStore } from "../../../store/useEditorStore";
 
 interface EditorButtonProps {
   text: string;
 }
 
 export default function EditorButton({ text }: EditorButtonProps) {
+  const { nextPage } = useEditorStore();
+
   return (
-    <motion.button className="p-4 bg-[#ff0000] rounded-md min-w-30 ml-auto h-13">
+    <motion.button
+      type="button"
+      onClick={nextPage}
+      className="p-4 bg-[#ff0000] rounded-md min-w-30 ml-auto h-13 cursor-pointer"
+    >
       {text.toUpperCase()}
     </motion.button>
   );
