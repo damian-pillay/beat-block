f7c04ee9886acb9a0abe41703cec712b8a83bef2
D4M14N L33 <damian.pillay@kcpivot.co.za>
2025-06-18 10:05:12 +0200

    feat: improve text input logic
    - add state and logic for number updating and text length limiting

--- frontend/src/pages/ProjectEditor/TextInput.tsx
+++ frontend/src/pages/ProjectEditor/TextInput.tsx
@@ -1,21 +1,49 @@
+import { useState, type ChangeEvent } from "react";
+
 type TextInputProps = {
   title: string;
   charLimit: number;
   placeholder?: string;
+  lineHeight?: number;
 };
 
 export default function TextInput({
   title,
   charLimit,
   placeholder,
+  lineHeight = 1,
 }: TextInputProps) {
+  const [value, setValue] = useState("");
+
+  function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    const newValue = e.target.value;
+
+    if (newValue.length <= charLimit) {
+      setValue(newValue);
+    }
+  }
+
+  const boxHeight = 2.6 + 2 * (lineHeight - 1);
+
   return (
     <section className="flex flex-col gap-2">
       <div className="flex justify-between">
         <h4 className="uppercase font-bold">{title}</h4>
-        <h4 className="text-right opacity-50">0/{charLimit}</h4>
+        <h4 className="text-right opacity-50">
+          {value.length}/{charLimit}
+        </h4>
       </div>
-      <input className="border rounded-sm p-2" placeholder={placeholder} />
+      <textarea
+        className={`
+          border border-white/50 focus:border-white
+          focus:outline-none
+          transition-colors duration-300 
+          rounded-md p-2 resize-none h-[${boxHeight}rem]
+        `}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => handleChange(e)}
+      />
     </section>
   );
 }
