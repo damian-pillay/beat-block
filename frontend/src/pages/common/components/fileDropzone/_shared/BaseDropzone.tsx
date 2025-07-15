e34b13a37fe3138afdfafbe863b55db52639080e
Damian Lee Pillay <damian.pillay@kcpivot.co.za>
2025-07-15 10:08:22 +0200

    feat: refactor dropzones using a base Dropzone

---
+++ frontend/src/pages/common/components/fileDropzone/_shared/BaseDropzone.tsx
@@ -0,0 +1,67 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { dropzoneConfig } from "../../../utils/dropzoneConfig";
+import useFileInput from "../../../hooks/useFileDrop";
+import { type DropzoneField } from "../../../types/dropzoneField";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+type FileDropZoneProps = {
+  field: DropzoneField;
+  isProjectUpload?: boolean;
+  onFilePresent: (file: File) => React.ReactNode;
+  onFileAbsent: () => React.ReactNode;
+};
+
+export default function BaseDropzone({
+  field,
+  isProjectUpload = false,
+  onFileAbsent,
+  onFilePresent,
+}: FileDropZoneProps) {
+  const [isDragOver, setIsDragOver] = useState(false);
+  const { file, handleFileInput } = useFileInput(field);
+  const navigate = useNavigate();
+
+  function handleDrop(event: React.DragEvent<HTMLElement>): void {
+    event.preventDefault();
+    setIsDragOver(false);
+
+    const files = Array.from(event.dataTransfer.files);
+
+    handleFileInput(files);
+
+    if (isProjectUpload) {
+      navigate("/");
+    }
+  }
+
+  const dropzoneStyle: string = !file
+    ? isDragOver
+      ? "bg-[#2a2929] border"
+      : "bg-[#1c1b1b] border-dashed"
+    : "bg-[#004222] border-green-400";
+
+  return (
+    <motion.section
+      onDrop={handleDrop}
+      onDragOver={(e: React.DragEvent<HTMLElement>) => {
+        e.preventDefault();
+        setIsDragOver(true);
+      }}
+      onDragEnter={(e: React.DragEvent<HTMLElement>) => {
+        e.preventDefault();
+        setIsDragOver(true);
+      }}
+      onDragLeave={() => {
+        setIsDragOver(false);
+      }}
+      className={`relative flex flex-col justify-center items-center rounded-2xl drag-none border-2 transition ${dropzoneStyle} ${
+        !isProjectUpload ? dropzoneConfig[field].styleClass : "h-full"
+      }`}
+    >
+      <AnimatePresence mode="wait">
+        {file ? onFilePresent(file) : onFileAbsent()}
+      </AnimatePresence>
+    </motion.section>
+  );
+}
--- frontend/src/pages/common/components/fileDropzone/projectDropzone/ProjectUploadDropzone.tsx
+++ frontend/src/pages/common/components/fileDropzone/projectDropzone/ProjectUploadDropzone.tsx
@@ -1,53 +1,14 @@
-import { AnimatePresence, motion } from "framer-motion";
-import useFileInput from "../../../hooks/useFileDrop";
+import BaseDropzone from "../_shared/BaseDropzone";
 import ProjectUploadPlaceholder from "./ProjectUploadPlaceholder";
 import { Check } from "lucide-react";
-import { useState } from "react";
 
 export default function ProjectUploadDropzone() {
-  const { file, handleFileInput } = useFileInput("compressedFile");
-  const [isDragOver, setIsDragOver] = useState(false);
-
-  function handleDrop(event: React.DragEvent<HTMLElement>): void {
-    event.preventDefault();
-    setIsDragOver(false);
-
-    const files = Array.from(event.dataTransfer.files);
-
-    handleFileInput(files);
-  }
-
-  const dropzoneStyle: string = !file
-    ? isDragOver
-      ? "bg-[#2a2929] border"
-      : "bg-[#1c1b1b] border-dashed"
-    : "bg-[#004222] border-green-400";
-
   return (
-    <motion.section
-      onDrop={handleDrop}
-      onDragOver={(e: React.DragEvent<HTMLElement>) => {
-        e.preventDefault();
-        setIsDragOver(true);
-      }}
-      onDragEnter={(e: React.DragEvent<HTMLElement>) => {
-        e.preventDefault();
-        setIsDragOver(true);
-      }}
-      onDragLeave={() => {
-        setIsDragOver(false);
-      }}
-      className={`relative flex flex-col justify-center items-center rounded-2xl drag-none border-2 transition ${dropzoneStyle} h-full`}
-    >
-      <AnimatePresence mode="wait">
-        {file ? (
-          <>
-            <Check size={50} />
-          </>
-        ) : (
-          <ProjectUploadPlaceholder />
-        )}
-      </AnimatePresence>
-    </motion.section>
+    <BaseDropzone
+      field={"compressedFile"}
+      isProjectUpload={true}
+      onFileAbsent={() => <ProjectUploadPlaceholder />}
+      onFilePresent={() => <Check size={50} />}
+    />
   );
 }
