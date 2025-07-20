9125e1b0527c796f0027ade5c386a3f6f7031792
beansbunny <damianleep@gmail.com>
2025-07-20 20:53:55 +0200

    feat: let Audio Player persist across pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,12 @@ import Home from "./pages/home/layout/Home";
 import ProjectEditor from "./pages/projectEditor/layout/ProjectEditor";
 import { Route, Routes, useLocation } from "react-router-dom";
 import MessageToast from "./pages/common/components/toast/MessageToast";
+import { useAudioPlayerStore } from "./pages/common/services/useAudioPlayerStore";
+import AudioPlayer from "./pages/common/components/audioPlayer/AudioPlayer";
 
 function App() {
   const location = useLocation();
+  const { isOpen } = useAudioPlayerStore();
 
   return (
     <>
@@ -15,6 +18,7 @@ function App() {
           <Route path="/create" element={<ProjectEditor />} />
         </Routes>
       </AnimatePresence>
+      <AnimatePresence>{isOpen && <AudioPlayer />}</AnimatePresence>
       <MessageToast />
     </>
   );
--- frontend/src/pages/home/layout/Home.tsx
+++ frontend/src/pages/home/layout/Home.tsx
@@ -3,22 +3,18 @@ import Dashboard from "../components/dashboard/Dashboard";
 import Navbar from "../../common/components/navBar/Navbar";
 import Catalog from "../components/catalogViewer/CatalogViewer";
 import ScreenTexture from "../../common/layout/ScreenTexture";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useProjectStore } from "../../projectEditor/services/useProjectStore";
 import { useEditorStore } from "../../projectEditor/services/useEditorStore";
-import AudioPlayer from "../../common/components/audioPlayer/AudioPlayer";
-import { useAudioPlayerStore } from "../../common/services/useAudioPlayerStore";
 
 function Home() {
   const { resetRequestForm: resetProject } = useProjectStore();
   const { resetPage } = useEditorStore();
-  const { isOpen, closePlayer } = useAudioPlayerStore();
 
   useEffect(() => {
     resetProject();
     resetPage();
-    closePlayer();
-  }, [resetPage, resetProject, closePlayer]);
+  }, [resetPage, resetProject]);
 
   return (
     <>
@@ -34,7 +30,6 @@ function Home() {
         >
           <Dashboard />
           <Catalog />
-          <AnimatePresence>{isOpen && <AudioPlayer />}</AnimatePresence>
         </motion.div>
       </div>
     </>
