e2685f9493b5bc37daf4b249f4e880aa944a3719
Damian Lee Pillay <damian.pillay@kcpivot.co.za>
2025-10-30 07:50:07 +0200

    feat: improve time data formatting

--- frontend/src/pages/home/components/projectViewerModal/ProjectTimeData.tsx
+++ frontend/src/pages/home/components/projectViewerModal/ProjectTimeData.tsx
@@ -1,19 +1,30 @@
 interface ProjectTimeDateProps {
-  timeCreated?: string;
-  timeUpdated?: string;
+  timeCreated: string;
+  timeUpdated: string;
 }
 
 export default function ProjectTimeData({
   timeCreated,
   timeUpdated,
 }: ProjectTimeDateProps) {
+  const createDate = new Date(timeCreated).toLocaleDateString("en-ZA", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const updateDate = new Date(timeUpdated).toLocaleDateString("en-ZA", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <section className="w-full flex justify-around font-montserrat font-normal text-[#848484] md:text-sm text-[0.55em]">
       <p id="date-created" className="italic">
-        Created: {timeCreated}
+        Created: {createDate}
       </p>
       <p id="date-updated" className="italic">
-        Updated: {timeUpdated}
+        Updated: {updateDate}
       </p>
     </section>
   );
